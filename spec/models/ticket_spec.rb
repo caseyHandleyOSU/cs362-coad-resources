19e4f3a451ee66bc4d52a7240ba81b7d1a6ed6b9
elkayy <63310939+imelkayy@users.noreply.github.com>
2025-01-13 15:36:59 -0800

    Test all Ticket attributes

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,9 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+  let(:ticket) { Ticket.new }
 
-  it "ticket exists" do 
-    Ticket.new
+  it "has a name" do
+    expect(ticket).to respond_to(:name)
+  end
+
+  it "has a description" do
+    expect(ticket).to respond_to(:description)
+  end
+
+  it "has a phone number" do
+    expect(ticket).to respond_to(:phone)
+  end
+
+  it "has a linked organization" do
+    expect(ticket).to respond_to(:organization_id)
+  end
+
+  it "has a closed status" do
+    expect(ticket).to respond_to(:closed)
+  end
+
+  it "has a closed date" do
+    expect(ticket).to respond_to(:closed_at)
+  end
+
+  it "has a resource category" do
+    expect(ticket).to respond_to(:resource_category_id)
+  end
+
+  it "has a region" do
+    expect(ticket).to respond_to(:region)
   end
 
 end
