62d47992563d0c3cff053120c3517d90ba97ff64
elkayy <63310939+imelkayy@users.noreply.github.com>
2025-01-13 16:32:30 -0800

    Add Resopurce Category association tests

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe ResourceCategory, type: :model do
     ResourceCategory.new
   end
 
+  it "has and belongs to many organizations" do
+    should have_and_belong_to_many(:organizations)
+  end
+
+  it "should have many tickets" do
+    should have_many(:tickets)
+  end
+
 end
