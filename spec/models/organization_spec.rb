d5c23550ab5edc87ff592f3d9438c673b41897b7
Casey <handlcas@oregonstate.edu>
2025-01-22 14:28:53 -0800

    Presence validation tests

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -104,4 +104,35 @@ RSpec.describe Organization, type: :model do
 
   end
 
+  describe "validate presense of" do
+    
+    it "email" do
+      should validate_presence_of(:email)
+    end
+
+    it "name" do
+      should validate_presence_of(:name)
+    end
+
+    it "phone" do
+      should validate_presence_of(:phone)
+    end
+
+    it "status" do
+      should validate_presence_of(:status)
+    end
+
+    it "primary name" do
+      should validate_presence_of(:primary_name)
+    end
+
+    it "secondary name" do
+      should validate_presence_of(:secondary_name)
+    end
+
+    it "secondary phone" do
+      should validate_presence_of(:secondary_phone)
+    end
+
+  end
 end
