fe2cba50f965119c008dcccfc55e93f19162ced5
Casey <handlcas@oregonstate.edu>
2025-01-27 15:07:29 -0800

    Application helper test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  BASE_TITLE = 'Disaster Resource Network'
+
+  describe "tests application" do
+
+    it "full title" do
+
+      expect(helper.full_title).to eq(BASE_TITLE)
+      test_title = "Hello, World"
+      expect(helper.full_title(test_title)).to eq("#{test_title} | #{BASE_TITLE}")
+
+    end
+    
+  end
+
 end
