cd55b3b4decf8bfdfc8bec18bdec720e0ed9a33a
Casey <handlcas@oregonstate.edu>
2025-02-05 09:23:30 -0800

    Convert ticket spec to use Factories

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,81 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  let(:ticket) do
-    region = Region.create!(name: "region1")
-    resource = ResourceCategory.create!(name: "resource1")
-
-    Ticket.create!(
-      name: "ticket",
-      phone: "+1-555-555-1212",
-      region_id: region.id,
-      resource_category_id: resource.id,
-      closed: false
-    )
-  end
-
-  let(:ticket_closed) do
-    region = Region.create!(name: "region2")
-    resource = ResourceCategory.create!(name: "resource2")
-
-    Ticket.create!(
-      name: "ticket",
-      phone: "+1-555-555-1212",
-      region_id: region.id,
-      resource_category_id: resource.id,
-      closed: true
-    )
-  end
-    
-  let(:ticket_org) do
-    region = Region.create!(name: "region3")
-    resource = ResourceCategory.create!(name: "resource3")
-    org = Organization.create!(
-      name: "myOrg1", email: "e@e.com", description: "", phone: "+15551234567",
-      primary_name: "1", secondary_name: "1", secondary_phone: "+15551234567")
-
-    Ticket.create!(
-      name: "ticket",
-      phone: "+1-555-555-1212",
-      region_id: region.id,
-      resource_category_id: resource.id,
-      closed: false,
-      organization_id: org.id
-    )
-  end
-
   describe "attributes" do
+    before(:each) do
+      @ticket = FactoryBot.build_stubbed(:ticket_open)
+    end
 
     it "has a name" do
-      expect(ticket).to respond_to(:name)
+      expect(@ticket).to respond_to(:name)
     end
 
     it "has a description" do
-      expect(ticket).to respond_to(:description)
+      expect(@ticket).to respond_to(:description)
     end
 
     it "has a phone number" do
-      expect(ticket).to respond_to(:phone)
+      expect(@ticket).to respond_to(:phone)
     end
 
     it "has a linked organization" do
-      expect(ticket).to respond_to(:organization_id)
+      expect(@ticket).to respond_to(:organization_id)
     end
 
     it "has a closed status" do
-      expect(ticket).to respond_to(:closed)
+      expect(@ticket).to respond_to(:closed)
     end
 
     it "has a closed date" do
-      expect(ticket).to respond_to(:closed_at)
+      expect(@ticket).to respond_to(:closed_at)
     end
 
     it "has a resource category" do
-      expect(ticket).to respond_to(:resource_category_id)
+      expect(@ticket).to respond_to(:resource_category_id)
     end
 
     it "has a region" do
-      expect(ticket).to respond_to(:region)
+      expect(@ticket).to respond_to(:region)
     end
 
   end
@@ -135,18 +95,22 @@ RSpec.describe Ticket, type: :model do
 
   describe "validate pattern of" do
 
-    describe "phone" do
+    before(:each) do
+      @ticket = FactoryBot.build_stubbed(:ticket_open)
+    end
+
+    describe "phone number" do
 
       it "valid" do
-        expect(ticket).to allow_value("+15555551212").for(:phone)
-        expect(ticket).to allow_value("15555551212").for(:phone)
-        expect(ticket).to allow_value("+15554567890").for(:phone)
+        expect(@ticket).to allow_value("+15555551212").for(:phone)
+        expect(@ticket).to allow_value("15555551212").for(:phone)
+        expect(@ticket).to allow_value("+15554567890").for(:phone)
       end
 
       it "invalid" do
-        expect(ticket).to_not allow_value("1").for(:phone)
-        expect(ticket).to_not allow_value("1234567890").for(:phone)
-        expect(ticket).to_not allow_value("5554567890").for(:phone)
+        expect(@ticket).to_not allow_value("1").for(:phone)
+        expect(@ticket).to_not allow_value("1234567890").for(:phone)
+        expect(@ticket).to_not allow_value("5554567890").for(:phone)
       end
 
     end
@@ -155,67 +119,102 @@ RSpec.describe Ticket, type: :model do
 
   describe "test static method" do
 
+    before(:each) do
+      @open_ticket = FactoryBot.build_stubbed(:ticket_open)
+    end
+
     it "open?" do
-      expect(ticket.open?).to be_truthy() 
-      expect(ticket_closed.open?).to be_falsy()
+      closed_ticket = FactoryBot.build_stubbed(:ticket_closed)
+
+      expect(@open_ticket.open?).to be_truthy() 
+      expect(closed_ticket.open?).to be_falsy()
     end
 
     it "captured?" do
-      expect(ticket.captured?).to be_falsy()
-      expect(ticket_org.captured?).to be_truthy()
+      org = FactoryBot.create(:organization)
+      org_ticket = FactoryBot.build_stubbed(
+        :ticket_open, 
+        organization_id: org.id
+      )
+
+      expect(@open_ticket.captured?).to be_falsy()
+      expect(org_ticket.captured?).to be_truthy()
     end
 
     it "to_s" do
-      expect(ticket.to_s).to eq("Ticket 1")
-      expect(ticket_closed.to_s).to eq("Ticket 2")
-      expect(ticket_org.to_s).to eq("Ticket 3")
+      ticket_1 = FactoryBot.build_stubbed(:ticket_open, name: "Ticket 1", id: 1)
+      ticket_2 = FactoryBot.build_stubbed(:ticket_closed, name: "Ticket 2", id: 2)
+
+      expect(ticket_1.to_s).to eq("Ticket 1")
+      expect(ticket_2.to_s).to eq("Ticket 2")
     end
 
   end
 
   describe "scope of" do
+    before(:each) do
+      @org = FactoryBot.create(:organization)
+      @reg = FactoryBot.create(:region)
+      @cat = FactoryBot.create(:resource_category)
+
+      @open_ticket = FactoryBot.create(:ticket_open,
+        region_id: @reg.id,
+        resource_category_id: @cat.id
+      )
+      @closed_ticket = FactoryBot.create(:ticket_closed,
+        region_id: @reg.id,
+        resource_category_id: @cat.id
+      )
+      @org_ticket = FactoryBot.create(
+        :ticket_open, 
+        organization_id: @org.id,
+        region_id: @reg.id,
+        resource_category_id: @cat.id
+      )
+    end
 
     it "closed tickets" do
-      expect(Ticket.closed).to include(ticket_closed)
-      expect(Ticket.closed).not_to include(ticket)
-      expect(Ticket.closed).not_to include(ticket_org)
+      expect(Ticket.closed).to include(@closed_ticket)
+      expect(Ticket.closed).not_to include(@open_ticket)
+      expect(Ticket.closed).not_to include(@org_ticket)
     end
 
     it "open tickets" do
-      expect(Ticket.open).to include(ticket)
-      expect(Ticket.open).not_to include(ticket_closed)
-      expect(Ticket.open).not_to include(ticket_org)
+      
+      expect(Ticket.open).to include(@open_ticket)
+      expect(Ticket.open).not_to include(@closed_ticket)
+      expect(Ticket.open).not_to include(@org_ticket)
     end
 
     it "tickets with an organization" do
-      expect(Ticket.all_organization).to include(ticket_org)
-      expect(Ticket.all_organization).not_to include(ticket)
-      expect(Ticket.all_organization).not_to include(ticket_closed)
+      expect(Ticket.all_organization).to include(@org_ticket)
+      expect(Ticket.all_organization).not_to include(@open_ticket)
+      expect(Ticket.all_organization).not_to include(@closed_ticket)
     end
 
     it "open tickets for an organization" do
-      expect(Ticket.organization(1)).to include(ticket_org)
-      expect(Ticket.organization(1)).not_to include(ticket)
-      expect(Ticket.organization(1)).not_to include(ticket_closed)
+      expect(Ticket.organization(@org.id)).to include(@org_ticket)
+      expect(Ticket.organization(@org.id)).not_to include(@open_ticket)
+      expect(Ticket.organization(@org.id)).not_to include(@closed_ticket)
     end
 
     it "closed tickets for an organization" do
-      expect(Ticket.closed_organization(1)).not_to include(ticket_org)
-      expect(Ticket.closed_organization(1)).not_to include(ticket)
-      expect(Ticket.closed_organization(1)).not_to include(ticket_closed)
-      expect(Ticket.closed_organization(1)).to be_empty
+      expect(Ticket.closed_organization(@org.id)).not_to include(@org_ticket)
+      expect(Ticket.closed_organization(@org.id)).not_to include(@open_ticket)
+      expect(Ticket.closed_organization(@org.id)).not_to include(@closed_ticket)
+      expect(Ticket.closed_organization(@org.id)).to be_empty
     end
 
     it "all tickets for a region" do
-      expect(Ticket.region(1)).to include(ticket)
-      expect(Ticket.region(1)).not_to include(ticket_org)
-      expect(Ticket.region(1)).not_to include(ticket_closed)
+      expect(Ticket.region(@reg.id)).to include(@open_ticket)
+      expect(Ticket.region(@reg.id)).to include(@org_ticket)
+      expect(Ticket.region(@reg.id)).to include(@closed_ticket)
     end
 
     it "all tickets for a resource category" do
-      expect(Ticket.resource_category(1)).to include(ticket)
-      expect(Ticket.resource_category(1)).not_to include(ticket_org)
-      expect(Ticket.resource_category(1)).not_to include(ticket_closed)
+      expect(Ticket.resource_category(@cat.id)).to include(@open_ticket)
+      expect(Ticket.resource_category(@cat.id)).to include(@org_ticket)
+      expect(Ticket.resource_category(@cat.id)).to include(@closed_ticket)
     end
 
   end
