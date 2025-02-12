7c1c7ffc4354a1a003fd80d070ffa020132fc7b1
Casey <handlcas@oregonstate.edu>
2025-02-12 15:00:42 -0800

    Implement dashboard controller tests

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,4 +2,47 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
+  let(:params) { {status: 'Open'} }
+
+  describe "as a logged out user" do
+    let(:user) { FactoryBot.create(:user) }
+    after(:each) { expect(response).not_to be_successful()}
+
+    it "test index" do
+      get(
+        :index,
+        params: params
+      )
+    end
+
+  end
+
+  describe "as a logged in user" do
+    let(:user) { FactoryBot.create(:user) }
+    before(:each) { sign_in user }
+    after(:each) { expect(response).to be_successful() }
+
+    it "test index" do
+      get(
+        :index,
+        params: params
+      )
+    end
+
+  end
+
+  describe "as an admin user" do
+    let(:user) { FactoryBot.create(:user, :admin) }
+    before(:each) { sign_in user }
+    after(:each) { expect(response).to be_successful() }
+
+    it "test index" do
+      get(
+        :index,
+        params: params
+      )
+    end
+
+  end
+  
 end
