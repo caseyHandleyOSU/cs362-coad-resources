867c3981857c3608e67ad90f4942e6cba0580e95
Casey <handlcas@oregonstate.edu>
2025-02-12 16:26:47 -0800

    Tickets Controller organization-user tests

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -75,4 +75,112 @@ RSpec.describe TicketsController, type: :controller do
     
   end
 
+  describe "as an organization user" do
+    let(:user) {  }
+    before(:each) { 
+      @org = FactoryBot.create(:organization, :approved)
+      user = FactoryBot.create(:user, organization_id: @org.id)
+      sign_in user
+    }
+
+    it "GET new" do
+      get(
+        :new
+      )
+
+      expect(response).to be_successful()
+    end
+
+    it "POST create" do
+      post(
+        :create,
+        params: {
+          ticket: FactoryBot.attributes_for(:ticket)
+        }
+      )
+
+      expect(response).to be_successful()
+    end
+
+    it "GET show" do
+      ticket = FactoryBot.create(:ticket)
+      get(
+        :show,
+        params: {
+          id: ticket.id
+        }
+      )
+
+      expect(response).to be_successful()
+    end
+
+    it "POST capture" do
+      ticket = FactoryBot.create(:ticket)
+      post(
+        :capture,
+        params: {
+          id: ticket.id
+        }
+      )
+
+      expect(response).to redirect_to(dashboard_path << "#tickets:open")
+    end
+
+    describe "POST release" do
+
+      it "owned ticket" do
+        ticket = FactoryBot.create(:ticket, organization_id: @org.id)
+        post(
+          :release,
+          params: {
+            id: ticket.id
+          }
+        )
+
+        expect(response).to redirect_to(dashboard_path << "#tickets:organization")
+      end
+
+      it "other's ticket" do
+        ticket = FactoryBot.create(:ticket)
+        post(
+          :release,
+          params: {
+            id: ticket.id
+          }
+        )
+
+        expect(response).to be_successful()
+      end
+
+    end
+
+    describe "PATCH close" do
+
+      it "owned ticket" do
+        ticket = FactoryBot.create(:ticket, organization_id: @org.id)
+        patch(
+          :close,
+          params: {
+            id: ticket.id
+          }
+        )
+
+        expect(response).to redirect_to(dashboard_path << "#tickets:organization")
+      end
+
+      it "other's ticket" do
+        ticket = FactoryBot.create(:ticket)
+        patch(
+          :close,
+          params: {
+            id: ticket.id
+          }
+        )
+
+        expect(response).to be_successful()
+      end
+
+    end
+    
+  end
 end
