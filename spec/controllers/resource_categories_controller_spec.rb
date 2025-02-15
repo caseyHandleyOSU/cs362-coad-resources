eb03c6d5b20b7afe6c9082b5bdb118db3dfebb3a
Casey <handlcas@oregonstate.edu>
2025-02-14 16:44:40 -0800

    Resouce category logged in (not admin) tests

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -104,4 +104,84 @@ RSpec.describe ResourceCategoriesController, type: :controller do
 
   end
 
+  describe "as a logged in user" do
+    let(:user) { FactoryBot.create(:user) }
+    before(:each) { sign_in user }
+    after(:each) { 
+      expect(response).not_to be_successful() 
+      expect(response).to redirect_to(dashboard_path)
+    }
+
+    it "GET index" do
+      get(
+        :index
+      )
+    end
+
+    it "GET show" do
+      cat = FactoryBot.create(:resource_category)
+      get(
+        :show,
+        params: {
+          id: cat.id
+        }
+      )
+    end
+
+    it "GET new" do
+      get(
+        :new
+      )
+    end
+
+    it "POST create" do
+      post(
+        :create,
+        params: {
+          resource_category: FactoryBot.attributes_for(:resource_category)
+        }
+      )
+    end
+
+    it "GET edit" do
+      cat = FactoryBot.create(:resource_category)
+      get(
+        :edit,
+        params: {
+          id: cat.id
+        }
+      )
+    end
+
+    it "PUT update" do
+      cat = FactoryBot.create(:resource_category)
+      put(
+        :update,
+        params: {
+          id: cat.id,
+          resource_category: FactoryBot.attributes_for(:resource_category)
+        }
+      )
+    end
+
+    it "PATCH update" do
+      cat = FactoryBot.create(:resource_category)
+      patch(
+        :update,
+        params: {
+          id: cat.id,
+          resource_category: FactoryBot.attributes_for(:resource_category)
+        }
+      )
+    end
+
+    it "PATCH activate" do
+      cat = FactoryBot.create(:resource_category)
+      patch(
+        :activate,
+        params: {
+          id: cat.id
+        }
+      )
+    end
 end
