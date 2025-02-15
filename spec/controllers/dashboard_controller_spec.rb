b8fe8e7ef0c717529e7951ce823f0525c84aa4d7
Casey <handlcas@oregonstate.edu>
2025-02-15 11:39:34 -0800

    Create shared_examples for dashboard controller spec

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -2,47 +2,50 @@ require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
 
-  let(:params) { {status: 'Open'} }
+  shared_examples "dashboard controller tests" do
 
-  describe "as a logged out user" do
-    let(:user) { FactoryBot.create(:user) }
-    after(:each) { expect(response).not_to be_successful()}
+    describe "as a logged out user" do
+      let(:user) { FactoryBot.create(:user) }
+      after(:each) { expect(response).not_to be_successful()}
 
-    it "test index" do
-      get(
-        :index,
-        params: params
-      )
+      it "test index" do
+        get(
+          :index,
+          params: params
+        )
+      end
+
+    end
+
+    describe "as a logged in user" do
+      let(:user) { FactoryBot.create(:user) }
+      before(:each) { sign_in user }
+      after(:each) { expect(response).to be_successful() }
+
+      it "test index" do
+        get(
+          :index,
+          params: params
+        )
+      end
+
+    end
+
+    describe "as an admin user" do
+      let(:user) { FactoryBot.create(:user, :admin) }
+      before(:each) { sign_in user }
+      after(:each) { expect(response).to be_successful() }
+
+      it "test index" do
+        get(
+          :index,
+          params: params
+        )
+      end
+
     end
 
   end
-
-  describe "as a logged in user" do
-    let(:user) { FactoryBot.create(:user) }
-    before(:each) { sign_in user }
-    after(:each) { expect(response).to be_successful() }
-
-    it "test index" do
-      get(
-        :index,
-        params: params
-      )
-    end
-
-  end
-
-  describe "as an admin user" do
-    let(:user) { FactoryBot.create(:user, :admin) }
-    before(:each) { sign_in user }
-    after(:each) { expect(response).to be_successful() }
-
-    it "test index" do
-      get(
-        :index,
-        params: params
-      )
-    end
-
   end
   
 end
