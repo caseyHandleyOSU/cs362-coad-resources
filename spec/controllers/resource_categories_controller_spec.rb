11cf23cd240fb10a366d6693cff73b03f237c78d
Casey <handlcas@oregonstate.edu>
2025-02-17 16:41:50 -0800

    Test resource category controller failure to create, activate, and deactivate

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -210,7 +210,6 @@ RSpec.describe ResourceCategoriesController, type: :controller do
   describe "as an admin user" do
     let(:user) { FactoryBot.create(:user, :admin) }
     before(:each) { sign_in user }
-    # after(:each) { expect(response).to be_successful() }
 
     it "GET index" do
       get(
@@ -237,14 +236,29 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to be_successful()
     end
 
-    it "POST create" do
-      post(
-        :create,
-        params: {
-          resource_category: FactoryBot.attributes_for(:resource_category)
-        }
-      )
-      expect(response).to redirect_to(resource_categories_path)
+    describe "POST create" do
+    
+      it "with a valid category" do
+        post(
+          :create,
+          params: {
+            resource_category: FactoryBot.attributes_for(:resource_category)
+          }
+        )
+        expect(response).to redirect_to(resource_categories_path)
+      end
+
+      it "with an invalid category" do
+        post(
+          :create,
+          params: {
+            resource_category: { name: nil }
+          }
+        )
+        expect(response).not_to redirect_to(resource_categories_path)
+        expect(response).to be_successful()
+      end
+
     end
 
     it "GET edit" do
@@ -258,16 +272,38 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to be_successful()
     end
 
-    it "PUT update" do
-      cat = FactoryBot.create(:resource_category)
-      put(
-        :update,
-        params: {
-          id: cat.id,
-          resource_category: FactoryBot.attributes_for(:resource_category)
-        }
-      )
-      expect(response).to redirect_to(cat)
+    describe "PUT update" do
+      
+      it "with a valid category" do
+        cat = FactoryBot.create(:resource_category)
+        put(
+          :update,
+          params: {
+            id: cat.id,
+            resource_category: FactoryBot.attributes_for(:resource_category)
+          }
+        )
+        expect(response).to redirect_to(cat)
+      end
+
+      it "with an invalid category" do
+        cat = FactoryBot.create(:resource_category)
+
+        expect_any_instance_of(ResourceCategory).
+          to receive(:update).
+          and_return(false)
+
+        put(
+          :update,
+          params: {
+            id: cat.id,
+            resource_category: FactoryBot.attributes_for(:resource_category)
+          }
+        )
+        expect(response).not_to redirect_to(cat)
+        expect(response).to be_successful()
+      end
+
     end
 
     it "PATCH update" do
@@ -282,26 +318,66 @@ RSpec.describe ResourceCategoriesController, type: :controller do
       expect(response).to redirect_to(cat)
     end
 
-    it "PATCH activate" do
-      cat = FactoryBot.create(:resource_category)
-      patch(
-        :activate,
-        params: {
-          id: cat.id
-        }
-      )
-      expect(response).to redirect_to(cat)
+    describe "PATCH activate" do
+      
+      it "a valid category" do
+        cat = FactoryBot.create(:resource_category)
+        patch(
+          :activate,
+          params: {
+            id: cat.id
+          }
+        )
+        expect(response).to redirect_to(cat)
+      end
+
+      it "an invalid category" do
+        cat = FactoryBot.create(:resource_category)
+
+        expect_any_instance_of(ResourceCategory).
+          to receive(:activate).
+          and_return(false)
+
+        patch(
+          :activate,
+          params: {
+            id: cat.id
+          }
+        )
+        expect(response).to redirect_to(cat)
+      end
+
     end
 
-    it "PATCH deactivate" do
-      cat = FactoryBot.create(:resource_category)
-      patch(
-        :deactivate,
-        params: {
-          id: cat.id
-        }
-      )
-      expect(response).to redirect_to(cat)
+    describe "PATCH deactivate" do
+
+      it "a valid category" do
+        cat = FactoryBot.create(:resource_category)
+        patch(
+          :deactivate,
+          params: {
+            id: cat.id
+          }
+        )
+        expect(response).to redirect_to(cat)
+      end
+
+      it "an invalid category" do
+        cat = FactoryBot.create(:resource_category)
+
+        expect_any_instance_of(ResourceCategory).
+          to receive(:deactivate).
+          and_return(false)
+
+        patch(
+          :deactivate,
+          params: {
+            id: cat.id
+          }
+        )
+        expect(response).to redirect_to(cat)
+      end
+
     end
 
     it "DELETE destroy" do
