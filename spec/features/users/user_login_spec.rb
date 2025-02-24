3158ceb18811599900a33ecc68f5221ef53a84a8
Casey <handlcas@oregonstate.edu>
2025-02-24 15:13:25 -0800

    Implement user login feature tests

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
 
+  it 'logs in an existing user successfully' do
+    user = FactoryBot.create(:user)
+    log_in_as(user)
+
+    expect(current_path).to eq(dashboard_path)
+  end
+
+  it 'does not log in an invalid user' do
+    user = FactoryBot.build(:user)
+    log_in_as(user)
+
+    expect(current_path).to eq(new_user_session_path)
+  end
+
 end
