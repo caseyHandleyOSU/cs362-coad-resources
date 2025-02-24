ea47ad829ecec1c2380d52385985521ec1d03b42
Casey <handlcas@oregonstate.edu>
2025-02-24 14:59:49 -0800

    Implement organization rejection feature test

--- spec/features/organizations/reject_organization_spec.rb
+++ spec/features/organizations/reject_organization_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Rejecting an organization', type: :feature do
 
+  before(:each) do
+    @organization = FactoryBot.create(:organization, :unapproved)
+    @user = FactoryBot.create(:user, :admin)
+    log_in_as(@user)
+  end
+
+  it 'successfully approves an organization' do
+    visit(organizations_path)
+
+    click_on(@organization.name)
+    expect(current_path).to eq(organization_path(@organization.id))
+    click_on('Reject')
+
+    expect(current_path).to eq(organizations_path)
+  end
+
 end
