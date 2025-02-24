aceb5031ab26738a6c255ff86484e44364088ca6
Casey <handlcas@oregonstate.edu>
2025-02-24 14:50:16 -0800

    Implement region creation feature testing

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+  before(:each) do
+    user = FactoryBot.create(:user, :admin)
+    log_in_as(user)
+  end
+
+  it 'creates a new region' do
+    visit(new_region_path)
+
+    fill_in('Name', with: 'My Cool Region!')
+    click_on('Add Region')
+
+    expect(current_path).to eq(regions_path)
+  end
+
 end
