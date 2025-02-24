0aa2e3b4fad40d357b8e9cac2180a2867e9047dc
Casey <handlcas@oregonstate.edu>
2025-02-24 15:23:57 -0800

    Implement organization update feature test

--- spec/features/organizations/update_organization_spec.rb
+++ spec/features/organizations/update_organization_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Updating an Organization', type: :feature do
 
+  before(:each) do
+    @organization = FactoryBot.create(:organization, :approved)
+    @user = FactoryBot.create(:user, organization_id: @organization.id)
+    log_in_as(@user)
+  end
+
+  it 'successfully approves an organization' do
+    visit(dashboard_path)
+
+    click_on('Edit Organization')
+    fill_in('Name', with: 'My New Name')
+    expect(current_path).to eq(edit_organization_path(@organization.id))
+    click_on('Update Resource')
+
+    expect(current_path).to eq(organization_path(@organization.id))
+  end
+
 end
