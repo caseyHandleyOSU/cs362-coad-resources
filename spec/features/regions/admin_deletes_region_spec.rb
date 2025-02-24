076ef214bb6f88ab6ef62991d0564377eb36a5b3
Casey <handlcas@oregonstate.edu>
2025-02-24 14:54:45 -0800

    Implement feature test for region deletion

--- spec/features/regions/admin_deletes_region_spec.rb
+++ spec/features/regions/admin_deletes_region_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Region', type: :feature do
 
+  before(:each) do
+    @region = FactoryBot.create(:region)
+    @user = FactoryBot.create(:user, :admin)
+    log_in_as(@user)
+  end
+
+  it 'deletes an existing region successfully' do
+    visit(regions_path)
+
+    click_on(@region.name)
+    expect(current_path).to eq(region_path(@region.id))
+    click_on('Delete')
+
+    expect(current_path).to eq(regions_path)
+  end
+
 end
