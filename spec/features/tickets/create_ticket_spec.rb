b1bc41588b986334d6efdccb3760a6f78aa8a97e
Casey <handlcas@oregonstate.edu>
2025-02-24 14:50:44 -0800

    Implement homepage ticket creation testing

--- spec/features/tickets/create_ticket_spec.rb
+++ spec/features/tickets/create_ticket_spec.rb
@@ -2,4 +2,45 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Ticket', type: :feature do
 
+  before(:each) do
+    @region = FactoryBot.create(:region)
+    @resource = FactoryBot.create(:resource_category)
+    @user = FactoryBot.create(:user)
+    log_in_as(@user)
+  end
+  
+  it 'can be created from the home screen' do
+    visit(root_path)
+
+    click_on('Get Help')
+
+    fill_in('Full Name', with: 'Test Name')
+    fill_in('Phone Number', with: '+1-555-123-4567')
+    fill_in('Description', with: 'My Description')
+
+    select(@region.name, from: 'Region')
+    select(@resource.name, from: 'Resource Category')
+    
+    click_on('Send this help request')
+
+    expect(current_path).to eq(ticket_submitted_path)
+  end
+
+  it 'cannot be created with an invalid phone number from the home screen' do
+    visit(root_path)
+
+    click_on('Get Help')
+
+    fill_in('Full Name', with: 'Test Name')
+    fill_in('Phone Number', with: '555-123-4567')
+    fill_in('Description', with: 'My Description')
+
+    select(@region.name, from: 'Region')
+    select(@resource.name, from: 'Resource Category')
+    
+    click_on('Send this help request')
+
+    expect(current_path).to eq(tickets_path)
+  end
+
 end
