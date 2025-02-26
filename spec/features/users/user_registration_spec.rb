3a31b99c59212302cfa5f4851489faaa0289d9b6
Casey <handlcas@oregonstate.edu>
2025-02-26 14:54:24 -0800

    fill out user registration feature test but don't make any assertions

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,4 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
+  before(:each) do
+    @dummy_usr = FactoryBot.build(:user)
+  end
+
+  it 'signs up a user successfully' do
+    visit(root_path)
+
+    click_on('Sign up')
+    fill_in('Email', with: @dummy_usr.email)
+    fill_in('Password', with: @dummy_usr.password)
+    fill_in('Password confirmation', with: @dummy_usr.password)
+
+    # CAPCHA doesn't work so this test will not work
+    # expect(current_path).to eq(dashboard_path)
+  end
+
 end
