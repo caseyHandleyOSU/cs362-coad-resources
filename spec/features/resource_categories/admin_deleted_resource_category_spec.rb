99a74419e2f9f5c399ab98e79565f5038455eee5
Casey <handlcas@oregonstate.edu>
2025-02-26 14:52:15 -0800

    Create resource category deletion feature test

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
 
+  before(:each) do
+    admin = FactoryBot.create(:user, :admin)
+    log_in_as(admin)
+    @resource = FactoryBot.create(:resource_category)
+  end
+
+  it 'admin deletes a resource category' do
+    visit(resource_category_path(@resource))
+    
+    click_on("Delete")
+
+    expect(current_path).to eq(resource_categories_path)
+    expect(body).not_to have_selector("h3", text: @resource.name)
+  end
+
 end
