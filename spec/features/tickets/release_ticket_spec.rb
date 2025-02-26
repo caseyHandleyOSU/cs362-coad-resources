eb13ef7378f2654b2cc09c581a0bd202d3b0b0b8
Casey <handlcas@oregonstate.edu>
2025-02-26 15:06:19 -0800

    Implement feature test for releasing tickets

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -2,4 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
 
+  before(:each) do
+    @org = FactoryBot.create(:organization, :approved)
+    @ticket = FactoryBot.create(:ticket, organization_id: @org.id)
+    @user = FactoryBot.create(:user, organization_id: @org.id)
+    log_in_as(@user)
+  end
+
+  it 'releases a ticket as a user with an organization' do
+    visit(dashboard_path)
+
+    click_on('Tickets')
+    expect(page).to have_selector('a', text: @ticket.name)
+    click_on(@ticket.name)
+    expect(current_path).to eq(ticket_path(@ticket))
+    click_on('Release')
+    
+    expect(current_path).to eq(dashboard_path)
+  end
+
 end
