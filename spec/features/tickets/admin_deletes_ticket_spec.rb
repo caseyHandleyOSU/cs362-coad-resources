2e73030f68eb03a9fe05b5fa2b6e29e2a1930dbf
Casey <handlcas@oregonstate.edu>
2025-02-26 14:59:35 -0800

    Create feature test for admin deletion of a ticket

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -2,4 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
 
+  before(:each) do
+    @ticket = FactoryBot.create(:ticket)
+    admin = FactoryBot.create(:user, :admin)
+    log_in_as(admin)
+  end
+
+  it 'deletes a ticket successfully' do
+    visit(dashboard_path)
+    
+    expect(page).to have_selector('a', text: @ticket.name)
+    click_on(@ticket.name)
+    click_on('Delete')
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).not_to have_selector('a', text: @ticket.name)
+  end
+
 end
