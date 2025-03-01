2d8afd3be1264a47df27ebb2eb592ab9012ac484
Casey <handlcas@oregonstate.edu>
2025-03-01 10:16:37 -0800

    Test capcha success and fail in registration feature tests

--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -2,21 +2,42 @@ require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
 
-  before(:each) do
-    @dummy_usr = FactoryBot.build(:user)
+  shared_examples 'sign up a user' do
+
+    before(:each) do
+      @dummy_usr = FactoryBot.build(:user)
+    end
+
+    it do
+      visit(root_path)
+
+      click_on('Sign up')
+      fill_in('Email', with: @dummy_usr.email)
+      fill_in('Password', with: @dummy_usr.password)
+      fill_in('Password confirmation', with: @dummy_usr.password)
+      
+      find('#commit').click
+      
+      expect(current_path).to eq(@destination)
+    end
   end
 
-  it 'signs up a user successfully' do
-    visit(root_path)
+  context 'with successful captcha' do
+    before(:each) { @destination = dashboard_path }
 
-    click_on('Sign up')
-    fill_in('Email', with: @dummy_usr.email)
-    fill_in('Password', with: @dummy_usr.password)
-    fill_in('Password confirmation', with: @dummy_usr.password)
-    
-    find('#commit').click
-    
-    expect(current_path).to eq(dashboard_path)
+    it_behaves_like 'sign up a user'
+  end
+
+  context 'with unsuccessful captcha' do
+    before(:each) do
+      expect_any_instance_of(Users::RegistrationsController).
+        to receive(:verify_recaptcha).
+        and_return(false) 
+
+      @destination = user_registration_path
+    end
+
+    it_behaves_like 'sign up a user'
   end
 
 end
