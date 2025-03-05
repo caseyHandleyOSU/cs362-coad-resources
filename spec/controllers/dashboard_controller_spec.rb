2b271f82f189c37f77ca011ae19cf3f6c2af3b79
purplestoke <>
2025-03-04 18:10:32 -0800

    fixed dashboard_controller spec so it tested elsif in status_options and removed un-neccessary tests

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
-    let(:user) { create(:user) }
+    let!(:user) { create(:user) }
 
     shared_examples 'dashboard controller tests' do
 
@@ -25,8 +25,8 @@ RSpec.describe DashboardController, type: :controller do
         describe 'logged in organization user' do
             before(:each) { 
                 org = create(:organization, :approved)
-                user = create(:user, organization_id: org)
-                sign_in user 
+                org_user = create(:user, organization_id: org.id)
+                sign_in org_user 
             }
             after(:each) { expect(response).to be_successful() }
 
@@ -37,8 +37,8 @@ RSpec.describe DashboardController, type: :controller do
 
         describe 'admin user' do
             before(:each) {
-                user = create(:user, :admin)
-                sign_in user 
+                admin = create(:user, :admin)
+                sign_in admin 
             }
             after(:each) { expect(response).to be_successful() }
 
@@ -83,88 +83,6 @@ RSpec.describe DashboardController, type: :controller do
             let(:params) { {status: ''} }
 
             it_behaves_like 'dashboard controller tests'
-        end
-    end
-
-    describe 'filtering tickets' do
-        let(:tickets_mock) { double("Tickets") }
-        let(:pagy_mock) { instance_double(Pagy) }
-
-        before do
-            allow(Ticket).to receive(:all).and_return(tickets_mock)
-            allow(controller).to receive(:pagy).with(tickets_mock, items: 10).and_return([pagy_mock, tickets_mock])
-        end
-
-        context 'when filtering tickets by status' do
-            before do
-                sign_in user
-                allow(Ticket).to receive(:open).and_return(tickets_mock)
-                allow(Ticket).to receive(:closed).and_return(tickets_mock)
-                allow(Ticket).to receive(:all_organization).and_return(tickets_mock)
-                allow(Ticket).to receive(:organization).and_return(tickets_mock)
-                allow(Ticket).to receive(:closed_organization).and_return(tickets_mock)
-            end
-
-            it "Open" do
-                allow(controller).to receive(:params).and_return(ActionController::Parameters.new({status: 'Open'}))
-                subject.index
-                expect(Ticket).to have_received(:open)
-            end
-
-            it "Closed" do
-                allow(controller).to receive(:params).and_return(ActionController::Parameters.new({status: 'Closed'}))
-                subject.index
-                expect(Ticket).to have_received(:closed)
-            end
-
-            it "My Closed'" do
-                allow(controller).to receive(:params).and_return(ActionController::Parameters.new({status: 'My Closed'}))
-                subject.index
-                expect(Ticket).to have_received(:closed_organization)
-            end
-
-            it "Captured" do
-                allow(controller).to receive(:params).and_return(ActionController::Parameters.new({status: 'Captured'}))
-                subject.index
-                expect(Ticket).to have_received(:all_organization)
-            end
-
-            it "My Captured" do
-                allow(controller).to receive(:params).and_return(ActionController::Parameters.new({status: 'My Captured'}))
-                subject.index
-                expect(Ticket).to have_received(:organization)
-            end
-        end
-
-        context 'when filtering by region and resource category' do
-            let(:filtered_tickets) { double("Filtered Tickets") }
-
-            before do
-                sign_in user
-                allow(Ticket).to receive(:all).and_return(tickets_mock)
-                allow(tickets_mock).to receive(:region).and_return(filtered_tickets)
-                allow(filtered_tickets).to receive(:resource_category).and_return(filtered_tickets)
-            end
-
-            it 'filters tickets by region and resource category if params are present' do
-               allow(controller).to receive(:params).and_return(ActionController::Parameters.new({region_id: "1", resource_category_id: "2"}))
-               subject.index
-               expect(tickets_mock).to have_received(:region).with("1")
-               expect(filtered_tickets).to have_received(:resource_category).with("2")
-            end
-        end
-
-        context 'sorting tickets' do
-            before do
-                sign_in user
-                allow(tickets_mock).to receive(:reverse).and_return(tickets_mock)
-            end
-
-            it "reverses the order if sort_order is 'Newest First'" do
-                allow(controller).to receive(:params).and_return(ActionController::Parameters.new({sort_order: 'Newest First'}))
-                subject.index
-                expect(tickets_mock).to have_received(:reverse)
-            end
         end
     end
 end
