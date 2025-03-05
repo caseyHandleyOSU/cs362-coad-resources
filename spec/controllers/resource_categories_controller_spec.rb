c9824f82e1e05385feb1ee2e297dbd831b0d3bbb
purplestoke <>
2025-03-05 12:22:43 -0800

    update resource_categories_controller to be more organized, use factories, decrease stub/mocks

--- spec/controllers/resource_categories_controller_spec.rb
+++ spec/controllers/resource_categories_controller_spec.rb
@@ -1,182 +1,355 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategoriesController, type: :controller do
-    let(:user) { instance_double(User, admin?: true) }
-    let(:resource_category) { instance_double(ResourceCategory, id: 1, name: "Test Category") }
+    let!(:user) { create(:user) }
+    let!(:admin) { create(:user, :admin)}
+    let!(:resource_category) { create(:resource_category) }
 
-    before do
-        allow(controller).to receive(:authenticate_user!)
-        allow(controller).to receive(:authenticate_admin)
-        allow(controller).to receive(:current_user).and_return(user)
-        allow(ResourceCategory).to receive(:find).with(1).and_return(resource_category)
-    end
-
-    describe 'index' do
-        let(:categories_mock) { double("ResourceCategories") }
-
-        before do
-            allow(ResourceCategory).to receive(:all).and_return(categories_mock)
-            allow(categories_mock).to receive(:order).with(:name).and_return(categories_mock)
-        end
-
-        it 'fetches all resource categories ordered by name' do
-            subject.index
-            expect(ResourceCategory).to have_received(:all)
-            expect(categories_mock).to have_received(:order).with(:name)
-        end
-    end
-
-    describe 'new' do
-        before do
-            allow(ResourceCategory).to receive(:new).and_return(resource_category)
-        end
-
-        it 'initializes a new resource category' do
-            subject.new
-            expect(ResourceCategory).to have_received(:new)
-            expect(subject.instance_variable_get(:@resource_category)).to eq(resource_category)
-        end
-    end
-
-    describe 'create' do
-        let(:new_resource_category) { instance_double(ResourceCategory, save: true) }
-
-        before do
-            allow(ResourceCategory).to receive(:new).and_return(new_resource_category)
-            allow(controller).to receive(:params).and_return(ActionController::Parameters.new({ resource_category: { name: "New Category" } }))
-        end
-
-        context 'when the resource category is successfully created' do
-            before do
-                allow(new_resource_category).to receive(:save).and_return(true)
-            end
-
-            it 'redirects to the index with a success message' do
-                expect(subject).to receive(:redirect_to).with(resource_categories_path, notice: 'Category successfully created.')
-                subject.create
-            end
-        end
-
-        context 'when the resource category creation fails' do
-            before do
-                allow(new_resource_category).to receive(:save).and_return(false)
-            end
-
-            it 'renders the new template' do
-                expect(subject).to receive(:render).with(:new)
-                subject.create
-            end
-        end
-    end
-
-    describe 'update' do
-        before do
-            allow(controller).to receive(:params).and_return(ActionController::Parameters.new({ id: 1, resource_category: { name: "Updated Category" } }))
-            allow(ResourceCategory).to receive(:find).with(1).and_return(resource_category)
-        end
-
-        context 'when the update is successful' do
-            before do
-                allow(resource_category).to receive(:update).and_return(true)
-            end
-
-            it 'redirects to the resource category with a success message' do
-                expect(subject).to receive(:redirect_to).with(resource_category, notice: 'Category successfully updated.')
-                subject.send(:set_resource_category)
-                subject.update
-                expect(resource_category).to have_received(:update).with({ "name" => "Updated Category" })
-            end
-        end
-
-        context 'when the update fails' do
-            before do
-                allow(resource_category).to receive(:update).and_return(false)
-            end
-
-            it 'renders the edit template' do
-                expect(subject).to receive(:render).with(:edit)
-                subject.send(:set_resource_category)
-                subject.update
-                expect(resource_category).to have_received(:update).with({ "name" => "Updated Category" })
-            end
-        end
-    end
-
-    describe 'activate' do
-        before do
-            allow(controller).to receive(:params).and_return(ActionController::Parameters.new({ id: "1" }))
-            allow(ResourceCategory).to receive(:find).with("1").and_return(resource_category)
-        end
+    describe 'GET index' do
         
-        context 'when activation is successful' do
-            before do
-                allow(resource_category).to receive(:activate).and_return(true)
-            end
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                get(:index)
 
-            it 'redirects with a success message' do
-                expect(subject).to receive(:redirect_to).with(resource_category, notice: 'Category activated.')
-                subject.send(:set_resource_category)
-                subject.activate
+                expect(response).to redirect_to(new_user_session_path)
             end
         end
 
-        context 'when activation fails' do
-            before do
-                allow(resource_category).to receive(:activate).and_return(false)
-            end
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
 
-            it 'redirects with an alert message' do
-                expect(subject).to receive(:redirect_to).with(resource_category, alert: 'There was a problem activating the category.')
-                subject.send(:set_resource_category)
-                subject.activate
+                get(:index)
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+            it 'verifies the request was a success' do
+                sign_in admin
+
+                get(:index)
+
+                expect(response).to be_successful 
+            end
+        end 
+    end
+
+    describe 'GET show' do
+
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                get(:show, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
+
+                get(:show, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+            it 'verifies the request was a success' do
+                sign_in admin
+
+                get(:show, params: {id: resource_category.id})
+
+                expect(response).to be_successful 
             end
         end
     end
 
-    describe 'deactivate' do
-        before do
-            allow(controller).to receive(:params).and_return(ActionController::Parameters.new({ id: "1" }))
-            allow(ResourceCategory).to receive(:find).with("1").and_return(resource_category)
-        end
-        context 'when deactivation is successful' do
-            before do
-                allow(resource_category).to receive(:deactivate).and_return(true)
-            end
+    describe 'GET new' do
 
-            it 'redirects with a success message' do
-                expect(subject).to receive(:redirect_to).with(resource_category, notice: 'Category deactivated.')
-                subject.send(:set_resource_category)
-                subject.deactivate
+        context 'as a logged out user' do
+            it 'redirects to login' do
+
+                get(:new)
+
+                expect(response).to redirect_to(new_user_session_path)
             end
         end
 
-        context 'when deactivation fails' do
-            before do
-                allow(resource_category).to receive(:deactivate).and_return(false)
-            end
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
 
-            it 'redirects with an alert message' do
-                expect(subject).to receive(:redirect_to).with(resource_category, alert: 'There was a problem deactivating the category.')
-                subject.send(:set_resource_category)
-                subject.deactivate
+                get(:new)
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+            it 'verifies the request was a success' do
+                sign_in admin
+
+                get(:new)
+
+                expect(response).to be_successful
             end
         end
     end
 
-    describe 'destroy' do
-        let(:delete_service) { instance_double(DeleteResourceCategoryService, run!: true) }
+    describe 'POST create' do
 
-        before do
-            allow(controller).to receive(:params).and_return(ActionController::Parameters.new({ id: "1" }))
-            allow(ResourceCategory).to receive(:find).with("1").and_return(resource_category)
-            allow(DeleteResourceCategoryService).to receive(:new).with(resource_category).and_return(delete_service)
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                post(:create, params: {resource_category: attributes_for(:resource_category)})
+
+                expect(response).to redirect_to(new_user_session_path)
+            end
         end
 
-        it 'runs the delete service and redirects with a success message' do
-            expect(delete_service).to receive(:run!)
-            expect(subject).to receive(:redirect_to).with(resource_categories_path, notice: "Category #{resource_category.name} was deleted.\nAssociated tickets now belong to the 'Unspecified' category.")
-            subject.send(:set_resource_category)
-            subject.destroy
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
+
+                post(:create, params: {resource_category: attributes_for(:resource_category)})
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+
+            context " when 'resource_category.save" do
+                it 'redirects to resource_categories_path' do
+                    sign_in admin
+
+                    post(:create, params: {resource_category: attributes_for(:resource_category)})
+
+                    expect(response).to redirect_to(resource_categories_path)
+                    expect(flash[:notice]).to eq('Category successfully created.')
+                end
+            end
+
+            context " when not 'resource_category.save" do
+                it 'renders the new template' do
+                    sign_in admin
+
+                    allow_any_instance_of(ResourceCategory).to receive(:save).and_return(false)
+
+                    expect(controller).to receive(:render).with(:new)
+
+                    post(:create, params: {resource_category: attributes_for(:resource_category)})
+                end
+            end
+        end
+    end 
+
+    describe 'GET edit' do
+
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                get(:edit, params: { id: resource_category.id })
+
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user 
+
+                get(:edit, params: { id: resource_category.id })
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+            it 'verifies the request was a success' do
+                sign_in admin
+
+                get(:edit, params: { id: resource_category.id })
+
+                expect(response).to be_successful
+            end
+        end
+    end
+
+    describe 'PUT update' do
+
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                put(:update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
+
+                put(:update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+
+            context "when resource_category.update(resource_category_params)" do
+                it 'redirects to resource_category' do
+                    sign_in admin
+
+                    put(:update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+
+                    expect(response).to redirect_to(resource_category)
+                    expect(flash[:notice]).to eq('Category successfully updated.')
+                end
+            end
+
+            context "when not resource_category.update(resource_category_params)" do
+                it 'renders the edit template' do
+                    sign_in admin
+
+                    allow_any_instance_of(ResourceCategory).to receive(:update).and_return(false)
+
+                    expect(controller).to receive(:render).with(:edit)
+
+                    put(:update, params: {id: resource_category.id, resource_category: attributes_for(:resource_category)})
+                end
+            end
+        end         
+    end
+
+    describe 'PUT activate' do
+
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                put(:activate, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
+
+                put(:activate, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+
+            context "when 'resource_category.activate" do
+                it 'redirects to resource_category' do
+                    sign_in admin
+
+                    put(:activate, params: {id: resource_category.id})
+
+                    expect(response).to redirect_to(resource_category)
+                    expect(flash[:notice]).to eq('Category activated.')
+                end
+            end
+
+            context "when not 'resource_category.activate" do
+                it 'redirects to resource category' do
+                    sign_in admin
+
+                    allow_any_instance_of(ResourceCategory).to receive(:activate).and_return(false)
+
+                    put(:activate, params: {id: resource_category.id})
+
+                    expect(response).to redirect_to(resource_category)
+                    expect(flash[:alert]).to eq('There was a problem activating the category.')
+                end
+            end
+        end
+    end
+
+    describe 'PUT deactivate' do
+
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                put(:deactivate, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
+
+                put(:deactivate, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+            
+            context "when 'resource_category.deactivate'" do
+                it 'redirects to resource_category' do
+                    sign_in admin
+
+                    put(:deactivate, params: {id: resource_category.id})
+
+                    expect(response).to redirect_to(resource_category)
+                    expect(flash[:notice]).to eq('Category deactivated.')
+                end
+            end
+
+            context "when not 'resource_category.deactivate" do
+                it 'redirects to resource_category with an alert' do
+                    sign_in admin
+
+                    allow_any_instance_of(ResourceCategory).to receive(:deactivate).and_return(false)
+
+                    put(:deactivate, params: {id: resource_category.id})
+
+                    expect(response).to redirect_to(resource_category)
+                    expect(flash[:alert]).to eq('There was a problem deactivating the category.')
+                end
+            end
+        end
+
+    end
+
+    describe 'DELETE destroy' do
+
+        context 'as a logged out user' do
+            it 'redirects to login' do
+                delete(:destroy, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(new_user_session_path)
+            end
+        end
+
+        context 'as a logged in user' do
+            it 'redirects to dashboard' do
+                sign_in user
+
+                delete(:destroy, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(dashboard_path)
+            end
+        end
+
+        context 'as an admin' do
+            it 'redirects to resource_categories_path' do
+                sign_in admin
+
+                delete(:destroy, params: {id: resource_category.id})
+
+                expect(response).to redirect_to(resource_categories_path)
+                expect(flash[:notice]).to eq("Category #{resource_category.name} was deleted.\nAssociated tickets now belong to the 'Unspecified' category.")
+            end
         end
     end
 end
