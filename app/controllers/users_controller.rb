c0a4f8265607efd7a355b8ef24ff9e3e7b8c5bdc
Casey <handlcas@oregonstate.edu>
2025-03-10 13:00:31 -0700

    Create all_emails()

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+
+  def all_emails
+    @users = User.all.order(:email)
+  end
 
 end
