79b39275a61fa0f6afc753ed4cbabb5feb93852b
Casey <handlcas@oregonstate.edu>
2025-03-10 12:54:47 -0700

    Create user email display feature test

---
+++ spec/features/users/user_emails_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Retrieving a list of user emails', type: :feature do
+
+  it 'as an admin user visit users' do
+    admin = FactoryBot.create(:user, :admin)
+    user2 = FactoryBot.create(:user)
+    log_in_as(admin)
+
+    visit(all_emails_path)
+
+    expect(page).to have_selector('p', text: @admin.email)
+    expect(page).to have_selector('p', text: @user2.email)
+  end
+
+end
